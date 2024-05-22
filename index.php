2418913e06c748e4ca19cbeec34b1b54d55a2800
TLLRSK <tullarisk@gmail.com>
2024-05-22 22:47:53 +0200

    added: modal style mobile and desktop (1920px)

--- index.php
+++ index.php
@@ -11,39 +11,54 @@
 <body>
     <article class="article">
         <h2 class="article-header">Escoge tu proyecto ideal y fináncialo en cómodas cuotas² Diferentes importes, mismos servicios incluidos</h2>
-        <div class="swiper-container">
+        <div class="swiper">
             <div class="swiper-wrapper project-carrousel">
                 <?php
-                    include './src/projects.php';
+                    include './public/projects.php';
                     foreach ($PROJECTS as $project):
                         $price_parts = explode(',', $project['price']);
                         $price_integer = $price_parts[0];
                         $price_decimal = isset($price_parts[1]) ? ',' . $price_parts[1] : '';
                 ?>
-                <div class="project-card swiper-slide" style="height:320px">
-                    <div class="project-image-container">
-                        <img class="project-image" src="<?php echo $project['image'] ?>" alt="<?php echo $project['name'] ?>">
+
+                    <div class="project-card swiper-slide" style="height:320px">
+                        <div class="project-image-container">
+                            <img class="project-image" src="<?php echo $project['image'] ?>" alt="<?php echo $project['name'] ?>">
+                        </div>
+                        <div class="project-info-container">
+                            <h3 class="project-name"><?php echo $project['name'] ?></h3>
+                            <p class="project-price">
+                                <?php echo $price_integer ?><span class="project-price--s"><?php echo $price_decimal ?></span>
+                                <span class="project-price--s">€/mes²</span>
+                            </p>
+                            <button class="btn--project-info">Más info</button>
+                        </div>
                     </div>
-                    <div class="project-info-container">
-                        <h3 class="project-name"><?php echo $project['name'] ?></h3>
-                        <p class="project-price">
-                            <?php echo $price_integer ?><span class="project-price--s"><?php echo $price_decimal ?></span>
-                            <span class="project-price--s">€/mes²</span>
-                        </p>
-                        <button class="btn--project-info">Más info</button>
-                    </div>
-                </div>
-            
-            <?php endforeach; ?>
+
+                 <?php endforeach; ?>
+
             </div>
+
             <div class="swiper-pagination"></div>
         </div>
+
+        <div class="modal">
+                <div class="modal-container">
+                    <h3 class="modal-project-name">Proyecto de interiorismo XS</h3>
+                    <svg class="modal-separator" width="100%" height="1">
+                        <line x1="0" y1="0.5" x2="100%" y2="0.5"/>
+                    </svg>
+                    <button class="btn--buy">¡Lo quiero!</button>
+                </div>
+                <div class="modal-overlay"></div>
+            </div>
+
     </article>
 </div>
 
     <script src="node_modules/swiper/swiper-bundle.min.js"></script>
     <script>
-        var swiper = new Swiper('.swiper-container', {
+        var swiper = new Swiper('.swiper', {
             spaceBetween: 15,
             breakpoints: {
                 1024: {
