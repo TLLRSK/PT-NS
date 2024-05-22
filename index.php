ef7874aa1ffe71b4d96958984a44f49362787a1f
TLLRSK <tullarisk@gmail.com>
2024-05-22 15:36:31 +0200

    added: swiper and carrousel with projects

--- index.php
+++ index.php
@@ -3,25 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Swiper Example</title>
+    
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="node_modules/swiper/swiper-bundle.min.css">
 </head>
 <body>
-<div class="container">
-    <?php
-        include './src/projects.php';
-        foreach ($PROJECTS as $project):
-    ?>
-        <div class="project-card">
-            <h3 class="project-name"><?php echo $project['name'] ?></h3>
-            <img class="project-image" src="<?php echo $project['image'] ?>" alt="<?php echo $project['name'] ?>">
-            <p class="project-price">
-                <?php echo $project['price'] ?>
-                <span class="project-price--span">€/mes²</span>
-            </p>
-            <button class="btn-project-info">Más información</button>
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <?php
+                include './src/projects.php';
+                foreach ($PROJECTS as $project):
+            ?>
+            <div class="project-card swiper-slide">
+                <h3 class="project-name"><?php echo $project['name'] ?></h3>
+                <img class="project-image" src="<?php echo $project['image'] ?>" alt="<?php echo $project['name'] ?>">
+                <p class="project-price">
+                    <?php echo $project['price'] ?>
+                    <span class="project-price--span">€/mes²</span>
+                </p>
+                <button class="btn-project-info">Más información</button>
+            </div>
+        
+        <?php endforeach; ?>
         </div>
-    
-    <?php endforeach; ?>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-pagination"></div>
+    </div>
+
+    <div class="container">
 </div>
+
+    <script src="node_modules/swiper/swiper-bundle.min.js"></script>
+    <script>
+        var swiper = new Swiper('.swiper-container', {
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+    </script>
 </body>
 </html>
